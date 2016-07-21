82df0b50b1d61b5b44afae9e7d13eae3399a4513
HienNguyen126 <hien_nguyen11@msn.com>
2016-07-21 23:21:45 +0700

    Add send email in proxy

---
+++ Proxy/src/java/User/SendEmail.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package User;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author Hien
+ */
+public class SendEmail extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String result = Send(email, password);
+        response(response, result);
+    }
+    
+    private void response(HttpServletResponse resp, String msg)
+			throws IOException {
+	PrintWriter out = resp.getWriter();
+	out.println(msg);
+	}
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    
+    public String Send(String email, String pass){
+        String re = "";
+           // Recipient's email ID needs to be mentioned.
+      String to = email;//change accordingly
+      // Sender's email ID needs to be mentioned
+      String from = "timerecordmanagement@gmail.com";//change accordingly
+      final String username = "timerecordmanagement";//change accordingly
+      final String password = "k19t2teamone";//change accordingly
+
+      // Assuming you are sending email through relay.jangosmtp.net
+      String host = "smtp.gmail.com";
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", host);
+      props.put("mail.smtp.port", "587");
+      // Get the Session object.
+      Session session = Session.getInstance(props,
+      new javax.mail.Authenticator() {
+         protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(username, password);
+         }
+      });
+      try {
+         // Create a default MimeMessage object.
+         Message message = new MimeMessage(session);
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+         // Set To: header field of the header.
+         message.setRecipients(Message.RecipientType.TO,
+         InternetAddress.parse(to));
+         // Set Subject: header field
+         message.setSubject("Get Your Account");
+         // Now set the actual message
+         message.setText("Hi there ! Welcome to Time Record Management! \n"
+                 +"Now you can access into your application with : \n"
+                 +"Your Email : " + email +"\n"
+                 +"Your Password : " + pass+"\n" 
+                 +"Best regards,\n"
+                 +"Develop team");
+        
+         // Send message
+         Transport.send(message);
+         re = "Ok";
+
+      } catch (MessagingException e) {
+            //throw new RuntimeException(e);
+            re = e.toString();
+      }
+        return re;
+    }
+}
